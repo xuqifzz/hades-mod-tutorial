5067d309fee1f456146fe088446405564562dd15
xu qi <65060782@qq.com>
2022-04-27 16:22:40 +0800

    开局自带心碎攻击和走火入魔

--- Scripts/RunManager.lua
+++ Scripts/RunManager.lua
@@ -302,6 +302,9 @@ function StartNewRun( prevRun, args )
 		CurrentRun.CurrentRoom = ChooseStartingRoom( CurrentRun, "Tartarus" )
 	end
 
+	AddTraitToHero({ TraitName = "AphroditeWeaponTrait", Rarity = "Epic" })
+	AddTraitToHero({ TraitName = "CharmTrait", Rarity = "Epic" })
+
 	return CurrentRun
 
 end
