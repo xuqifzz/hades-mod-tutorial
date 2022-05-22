1d0f69a5fc2a6b8100b0255ea00e2f3bce7ab86b
xu qi <65060782@qq.com>
2022-05-23 00:29:13 +0800

    第二节 Hello World! 显示和移动文字

--- Scripts/DamageLog.lua
+++ Scripts/DamageLog.lua
@@ -6,4 +6,23 @@ OnAnyLoad{ function()
     SetScaleX({ Id = objId, Fraction = 300 / 480 })
     SetScaleY({ Id = objId, Fraction = 300 / 267 })
 
+    CreateTextBox({ 
+        Id = objId, 
+        Text = "Hello World!", 
+        FontSize = 20, 
+        Color = {1,1,1,1}, 
+        Justification = "Left",
+        OffsetX = -50,
+        OffsetY = -100,
+     })
+
+     wait(2)
+     Move({ Ids = objId, Angle = 90, Distance = 200, Speed = 1000 })
+     wait(0.2)
+     Move({ Ids = objId, Angle = 180, Distance = 500, Speed = 1000 })
+     wait(0.5)
+     Move({ Ids = objId, Angle = 270, Distance = 200, Speed = 1000 })
+     wait(0.2)
+     Move({ Ids = objId, Angle = 0, Distance = 500, Speed = 1000 })
+
 end}
